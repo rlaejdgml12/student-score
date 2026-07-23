f3badac7860ddd18bb4eed35147c3f6adce32d0c
김동희 <rlaejdgml15@gmail.com>
2026-07-23 11:41:05 +0900

    feat : v3 캡슐화 적용 - 필드 private화, static final 상수, 점수 유효성 검증

--- src/app/StudentMain.java
+++ src/app/StudentMain.java
@@ -4,7 +4,7 @@ import domain.Student;
 
 public class StudentMain {
     public static void main(String[] args) {
-        Student student1 = new Student("김동희", 90, 85, 78);
+        Student student1 = new Student("김동희", 100, 85, 78);
 
         Student student2 = new Student("홍길동", 80, 75, 88);
 
--- src/domain/Student.java
+++ src/domain/Student.java
@@ -1,10 +1,13 @@
 package domain;
 
 public class Student {
-    String name;
-    int korScore;
-    int mathScore;
-    int engScore;
+    static final int MAX_SCORE = 100;
+    static final int MIN_SCORE = 0;
+
+    private String name;
+    private int korScore;
+    private int mathScore;
+    private int engScore;
 
     public Student(){
         this("이름없음", 0, 0, 0);
@@ -12,8 +15,29 @@ public class Student {
 
     public Student(String name, int korScore, int mathScore, int engScore){
         this.name = name;
+        setKorScore(korScore);
+        setMathScore(mathScore);
+        setEngScore(engScore);
+    }
+
+    public void setKorScore(int korScore){
+        if (korScore > MAX_SCORE || MIN_SCORE > korScore){
+            throw new IllegalArgumentException("점수는 0 ~ 100 사이여야 합니다 : " + korScore);
+        }
         this.korScore = korScore;
+    }
+
+    public void setMathScore(int mathScore){
+        if (mathScore > MAX_SCORE || MIN_SCORE > mathScore){
+            throw new IllegalArgumentException("점수는 0 ~ 100 사이여야 합니다 : " + mathScore);
+        }
         this.mathScore = mathScore;
+    }
+
+    public void setEngScore(int engScore){
+        if (engScore > MAX_SCORE || MIN_SCORE > engScore){
+            throw new IllegalArgumentException("점수는 0 ~ 100 사이여야 합니다 : " + engScore);
+        }
         this.engScore = engScore;
     }
 
