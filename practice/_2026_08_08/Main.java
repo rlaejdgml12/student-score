9a0ff2cbc96da93c1d5dc2a9c82e2e1b119be9cc
김동희 <rlaejdgml15@gmail.com>
2026-08-08 16:53:16 +0900

    chore : 8/8 HashSet 중복 제거 재현 드릴

--- practice/_2026_08_08/Main.java
+++ practice/_2026_08_08/Main.java
@@ -1,4 +1,15 @@
 package _2026_08_08;
 
+import java.util.HashSet;
+import java.util.Set;
+import studentscore.domain.Student;
+
 public class Main {
+    public static void main(String[] args) {
+        Set<Student> students = new HashSet<>();
+        students.add(new Student("김철수", 90, 90, 90));
+        students.add(new Student("김철수", 90, 90, 90));
+        System.out.println(students.size());
+    }
+
 }
